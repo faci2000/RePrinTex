bcc22bfedb5dd19945992651f046106872c2c13d
Alice <micek2000@gmail.com>
2021-03-22 11:57:08 +0100

    mały update

--- GUI.py
+++ GUI.py
@@ -1,9 +1,10 @@
 import sys
 
-from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QImage, QIcon
+from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMenuBar, \
-    QMenu, QAction, QFileDialog, QLabel, QMessageBox, QDockWidget, QListWidget
+    QMenu, QAction, QFileDialog, QLabel, QMessageBox, QDockWidget, QListWidget, QListWidgetItem, QScrollArea, \
+    QScrollBar, QSizePolicy
 
 
 # TODO:
@@ -27,12 +28,15 @@ class Window(QMainWindow):
         self.filePaths = None
 
         # Geometry
-        self.setGeometry(600, 300, 800, 600)
+        self.setGeometry(600, 300, 1000, 800)
         self.setWindowTitle("RePrinTex")
 
         # MenuBar
         # - File menu
         openAction = QAction("&Open files", self, shortcut="Ctrl+O", triggered=self.loadFiles)
+        zoomInAction = QAction("&Zoom in", self, shortcut="Ctrl+i", triggered=self.zoomIn)
+        zoomOutAction = QAction("&Zoom out", self, shortcut="Ctrl+u", triggered=self.zoomOut)
+
         saveAction = QAction("&Save", self, shortcut="Ctrl+S", triggered=self.saveImage)
         saveAllAction = QAction("&Save All", self, triggered=self.saveAllImages)
         saveAsAction = QAction("&Save as", self, triggered=self.saveImageAs)
@@ -40,19 +44,28 @@ class Window(QMainWindow):
         saveMenu.addActions([saveAction, saveAsAction, saveAllAction])
 
         fileMenu = QMenu("&File", self)
-        fileMenu.addActions([openAction])
+        fileMenu.addActions([openAction, zoomInAction, zoomOutAction])
         fileMenu.addMenu(saveMenu)
         self.menuBar().addMenu(fileMenu)
 
         # Central widget
-        self.setCentralWidget(QLabel())
+        self.scrollArea = QScrollArea(self)
+        self.setCentralWidget(self.scrollArea)
+        self.centralLabel = QLabel(self.scrollArea)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setWidget(self.centralLabel)
+        self.centralLabel.setAlignment(Qt.AlignCenter)
 
         # Dock widgets - to jest jakieś grube i chyba można tu zrobić sporo rzeczy
         self.dock1 = QDockWidget("Files", self)
-        self.dock1.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
+        self.dock1.setAllowedAreas(Qt.TopDockWidgetArea | Qt.BottomDockWidgetArea)
+        self.addDockWidget(Qt.BottomDockWidgetArea, self.dock1)
+
         self.filesList = QListWidget(self.dock1)
+        self.filesList.setViewMode(QListWidget.IconMode)
+        self.filesList.setResizeMode(QListWidget.Adjust)
+        self.filesList.setIconSize(QSize(150, 150))
         self.dock1.setWidget(self.filesList)
-        self.addDockWidget(Qt.RightDockWidgetArea, self.dock1)
 
         # Toolbars
 
@@ -63,12 +76,31 @@ class Window(QMainWindow):
     # po kliknięciu 'Open' zapisuje ścieżki do wybranych plików w filePaths, tworzy QListe filesList i otwiera pierwszy obrazek
     def loadFiles(self):
         filenames, _ = QFileDialog.getOpenFileNames(self)
-        self.filePaths = filenames
-        names = [file.split('/')[-1] for file in filenames] # to jest tylko po to, żeby trzymać w liscie ładne nazwy a nie całą ścieżke
-        self.filesList.clear()
-        self.filesList.addItems(names)
-        self.filesList.currentTextChanged[str].connect(self.changeImage)
-        self.openImage(filenames[0])
+        if filenames:
+            self.filePaths = filenames
+            names = [file.split('/')[-1] for file in filenames] # to jest tylko po to, żeby trzymać w liscie ładne nazwy a nie całą ścieżke
+            self.filesList.clear()
+
+            for i in range(len(names)):
+                item = QListWidgetItem(QIcon(QPixmap(self.filePaths[i])), names[i])
+                self.filesList.addItem(item)
+
+            self.filesList.currentTextChanged[str].connect(self.changeImage)
+            self.openImage(filenames[0])
+
+    def zoomIn(self):
+        size = self.currentPixmap.size()
+        size.setWidth(int(size.width() * 1.1))
+        size.setHeight(int(size.height() * 1.1))
+        self.currentPixmap = self.currentPixmap.scaled(size, transformMode=Qt.SmoothTransformation)
+        self.centralLabel.setPixmap(self.currentPixmap)
+
+    def zoomOut(self):
+        size = self.currentPixmap.size()
+        size.setWidth(int(size.width() * 0.9))
+        size.setHeight(int(size.height() * 0.9))
+        self.currentPixmap = self.currentPixmap.scaled(size, transformMode=Qt.SmoothTransformation)
+        self.centralLabel.setPixmap(self.currentPixmap)
 
     # otwiera obrazek o danym filename
     def openImage(self, filename):
@@ -78,7 +110,8 @@ class Window(QMainWindow):
                 QMessageBox.information(self, "Error", "Cannot load file {}.".format(filename))
                 return
             self.currentPixmap = QPixmap.fromImage(image).scaled(self.width(), self.height())
-            self.centralWidget().setPixmap(self.currentPixmap)
+            self.centralLabel.setPixmap(self.currentPixmap.scaled(
+                self.centralWidget().size(), Qt.KeepAspectRatio))
         else:
             QMessageBox.information(self, "Error", "Invalid file!")
 
