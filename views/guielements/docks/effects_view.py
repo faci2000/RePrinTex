9aedb2193317531ead6951582f012a39b726ecf4
faci2000 <faci20000@gmail.com>
2021-06-05 19:37:28 +0200

    Clean lambda expressions mess

--- views/guielements/docks/effects_view.py
+++ views/guielements/docks/effects_view.py
@@ -80,7 +80,6 @@ class EffectsView:
         return button
 
     def create_checkobxes(self):
-        # labels = [" ","Main lines","Minor lines","Text block","Words","Letters"]
         hbox = QHBoxLayout()
         vbox = QVBoxLayout()
         vbox.addWidget(QLabel(' '))
@@ -88,72 +87,28 @@ class EffectsView:
             vbox.addWidget(QLabel(label.value))
         hbox.addLayout(vbox)
 
+        def get_lambda(effect_type,type,org):
+                    return lambda: self.controller.change_effects(
+                        {'effect_type': effect_type, 'type': type, 'org': org})
+
         vbox = QVBoxLayout()
         vbox.addWidget(QLabel("Original"))
 
-        # for label in Lines:
-        #     label_str = label.value[:]
-        #     print(label_str)
-        #     chk = QCheckBox()
-        #     chk.stateChanged.connect(lambda: self.controller.change_effects({'effect_type':EffectType.LINES,'type':label_str,'org':True,'value':not chk.isChecked()}))
-        #     vbox.addWidget(chk)
-        # hbox.addLayout(vbox)
-
-        chk = QCheckBox()
-        chk.stateChanged.connect(lambda: self.controller.change_effects(
-            {'effect_type': EffectType.LINES, 'type': Lines.MAIN_LINES.value, 'org': True}))
-        vbox.addWidget(chk)
-        chk = QCheckBox()
-        chk.stateChanged.connect(lambda: self.controller.change_effects(
-            {'effect_type': EffectType.LINES, 'type': Lines.MINOR_LINES.value, 'org': True}))
-        vbox.addWidget(chk)
-        chk = QCheckBox()
-        chk.stateChanged.connect(lambda: self.controller.change_effects(
-            {'effect_type': EffectType.LINES, 'type': Lines.TEXT_BLOCK.value, 'org': True}))
-        vbox.addWidget(chk)
-        chk = QCheckBox()
-        chk.stateChanged.connect(lambda: self.controller.change_effects(
-            {'effect_type': EffectType.LINES, 'type': Lines.WORDS.value, 'org': True}))
-        vbox.addWidget(chk)
-        chk = QCheckBox()
-        chk.stateChanged.connect(lambda: self.controller.change_effects(
-            {'effect_type': EffectType.LINES, 'type': Lines.LETTERS.value, 'org': True}))
-        vbox.addWidget(chk)
+        for label in Lines:
+            chk = QCheckBox()
+            chk.stateChanged.connect(get_lambda(EffectType.LINES,label.value,True))
+            vbox.addWidget(chk)
         hbox.addLayout(vbox)
 
         vbox = QVBoxLayout()
         vbox.addWidget(QLabel("Preview"))
-        # x=[val.value for val in Lines]
-        # print(x)
-        # for i in range(len(x)):
-        #     chk = QCheckBox()
-        #     chk.stateChanged.connect(lambda: self.controller.change_effects({'effect_type':EffectType.LINES,'type':x[i],'org':False,'value':not chk.isChecked()}))
-        #     vbox.addWidget(chk)
-        # hbox.addLayout(vbox)
 
-        chk = QCheckBox()
-        chk.stateChanged.connect(lambda: self.controller.change_effects(
-            {'effect_type': EffectType.LINES, 'type': Lines.MAIN_LINES.value, 'org': False}))
-        vbox.addWidget(chk)
-        chk = QCheckBox()
-        chk.stateChanged.connect(lambda: self.controller.change_effects(
-            {'effect_type': EffectType.LINES, 'type': Lines.MINOR_LINES.value, 'org': False}))
-        vbox.addWidget(chk)
-        chk = QCheckBox()
-        chk.stateChanged.connect(lambda: self.controller.change_effects(
-            {'effect_type': EffectType.LINES, 'type': Lines.TEXT_BLOCK.value, 'org': False}))
-        vbox.addWidget(chk)
-        chk = QCheckBox()
-        chk.stateChanged.connect(lambda: self.controller.change_effects(
-            {'effect_type': EffectType.LINES, 'type': Lines.WORDS.value, 'org': False}))
-        vbox.addWidget(chk)
-        chk = QCheckBox()
-        chk.stateChanged.connect(lambda: self.controller.change_effects(
-            {'effect_type': EffectType.LINES, 'type': Lines.LETTERS.value, 'org': False}))
-        vbox.addWidget(chk)
+        for label in Lines:
+            chk = QCheckBox()
+            chk.stateChanged.connect(get_lambda(EffectType.LINES,label.value,False))
+            vbox.addWidget(chk)
         hbox.addLayout(vbox)
 
-        # print("returnig vbox")
         return hbox
 
 
